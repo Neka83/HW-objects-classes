4733eab37e960f4c3011f0ae405099a7bfabb609
Neka83 <n_neike@mail.ru>
2025-06-11 16:24:23 +0500

    Homework Objects & Classes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Лев", "Толстой");
+        Author author2 = new Author("Фёдор", "Достоевский");
 
+        Book book1 = new Book("Война и Мир", author1, 1869);
+        Book book2 = new Book("Преступление и наказание", author2, 1866);
+
+        System.out.println("Книга: " + book1.getTitle() + ", автор: " + book1.getAuthor().getFullName() + ", год публикации: " + book1.getPublishingYear());
+
+        System.out.println("Книга: " + book2.getTitle() + ", автор: " + book2.getAuthor().getFullName() + ", год публикации: " + book2.getPublishingYear());
+
+        book2.setPublishingYear(1870);
+
+        System.out.println("После изменения:");
+        System.out.println("Книга: " + book2.getTitle() + ", автор: " + book2.getAuthor().getFullName() + ", год публикации: " + book2.getPublishingYear());
     }
 }
